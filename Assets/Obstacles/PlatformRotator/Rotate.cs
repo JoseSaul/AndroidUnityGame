70ab64be4d0ed7032e20ce1c370650b1bda4b910
JoseSaul <40270646+JoseSaul@users.noreply.github.com>
2019-11-20 20:10:22 +0000

    PlatformRotator

---
+++ Assets/Obstacles/PlatformRotator/Rotate.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Obstacles.PlatformRotator
+{
+    public class Rotate : MonoBehaviour
+    {
+
+        [SerializeField] private GameObject platform;
+        [SerializeField] private bool right;
+        [SerializeField] private int speed;
+        
+        void Update()
+        {
+            if (right)
+            {
+                platform.transform.Rotate(new Vector3(0,0,Time.deltaTime*speed));
+            }
+            else
+            {
+                platform.transform.Rotate(new Vector3(0,0,Time.deltaTime*-speed));
+            }
+            
+        }
+        
+    
+    }
+}
