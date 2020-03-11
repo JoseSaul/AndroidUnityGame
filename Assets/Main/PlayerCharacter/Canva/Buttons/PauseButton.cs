b7782ee35088b285a427d2f0ec3dbeedf2ee5ccb
JoseSaul <40270646+JoseSaul@users.noreply.github.com>
2020-03-11 12:44:06 +0000

    Pause menu
    Pause button and menu

---
+++ Assets/Main/PlayerCharacter/Canva/Buttons/PauseButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PauseButton : MonoBehaviour,IPointerDownHandler
+{
+    [SerializeField] private Canvas pauseCanvas = new Canvas();
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pauseCanvas.gameObject.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    
+}
