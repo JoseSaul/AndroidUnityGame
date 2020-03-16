65db7222881301743a15b7888d76dace707f4431
JoseSaul <40270646+JoseSaul@users.noreply.github.com>
2020-03-16 13:29:39 +0000

    PauseMenu Finished

---
+++ Assets/Main/PlayerCharacter/Canva/PauseMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private Canvas pauseCanvas = new Canvas();
+
+    public void continu()
+    {
+        Time.timeScale = 1f; 
+        pauseCanvas.gameObject.SetActive(false);
+    }
+
+    public void exitGame()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene("MainMenu");
+    }
+}
+
